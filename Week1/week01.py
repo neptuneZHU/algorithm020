77aa0c57365bff4fe2bcac36c68f6c3ee6c4e704
zhu yuting <yuting002@e.ntu.edu.sg>
2020-11-22 16:26:01 +0800

    Update week01.py

--- Week1/week01.py
+++ Week1/week01.py
@@ -151,8 +151,119 @@ class Solution(object):
         else:
             return self.plusOne(digits[:-1])+[0]
 
+#622. Design Circular Queue
+class Node:
+    def __init__(self,value):
+        self.val = value
+        self.next = self.pre = None
+       
+class CircularQueue:
+    def __init__(self,k):
+        self.size = k
+        self.curSize = 0
+        self.head = self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+        
+    def enqueue(self,value):
+        if self.curSize < self.size:
+            node = Node(value)
+            node.pre = self.tail.pre
+            node.next = self.tail
+            node.pre.next = node.next.pre = node
+            self.curSize +=1
+            return True
+        return False
+    
+    def dequeue(self):
+        if self.curSize > 0:
+            node = self.head.next
+            node.pre.next = node.next
+            node.next.pre = node.pre
+            self.curSize -=1
+            return True
+        return False
+    
+    def Front(self):
+        return self.head.next.val
+    
+    def Rear(self):
+        return self.tail.pre.val
+    
+    def isEmpty(self):
+        return self.curSize == 0
+    
+    def isFull(self):
+        return self.curSize == self.size
+        
+
 # 641. Design Circular Deque (Medium)
- 
+class Node:
+    def __init__(self, value):
+        self.val = value
+        self.next = self.pre = None
+        
+class MyCircularDeque:
+
+    def __init__(self, k):
+        self.head = self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+        self.size = k
+        self.curSize = 0
+
+    def add(self, value, preNode):
+        new = Node(value)
+        new.pre = preNode
+        new.next = preNode.next
+        new.pre.next = new.next.pre = new
+        self.curSize += 1
+        
+    def remove(self, preNode):
+        node = preNode.next
+        node.pre.next = node.next
+        node.next.pre = node.pre
+        self.curSize -= 1
+    
+    def insertFront(self, value):
+        if self.curSize < self.size:
+            self.add(value, self.head)
+            return True
+        return False
+
+    def insertLast(self, value):
+        if self.curSize < self.size:
+            self.add(value, self.tail.pre)
+            return True
+        return False
+
+    def deleteFront(self):
+        if self.curSize:
+            self.remove(self.head)
+            return True
+        return False
+
+    def deleteLast(self):
+        if self.curSize:
+            self.remove(self.tail.pre.pre)
+            return True
+        return False
+
+    def getFront(self):
+        if self.curSize:
+            return self.head.next.val
+        return -1
+
+    def getRear(self):
+        if self.curSize:
+            return self.tail.pre.val
+        return -1
+
+    def isEmpty(self):
+        return self.curSize == 0
+
+    def isFull(self):
+        return self.curSize == self.size
 
 
 # 42. Trapping Rain Water (Hard)
